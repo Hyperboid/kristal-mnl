9521a9b10960b114614a0587cd35ce4caba55539
Hyperboid <23138363+Hyperboid@users.noreply.github.com>
2025-05-05 00:07:10 -0400

    Seperate mnlEncounter method

--- scripts/hooks/Game.lua
+++ scripts/hooks/Game.lua
@@ -12,35 +12,39 @@ local function r_includes(getter, check, class)
     end
 end
 
+function Game:mnlEncounter(encounter, transition, enemy, context)
+    if transition == nil then transition = true end
+
+    if self.battle then
+        error("Attempt to enter battle while already in battle")
+    end
+
+    if enemy and not isClass(enemy) then
+        self.encounter_enemies = enemy
+    else
+        self.encounter_enemies = {enemy}
+    end
+
+    self.state = "BATTLE"
+
+    self.battle = MNLBattle()
+
+    if context then
+        self.battle.encounter_context = context
+    end
+
+    if type(transition) == "string" then
+        self.battle:postInit(transition, encounter)
+    else
+        self.battle:postInit(transition and "TRANSITION" or "INTRO", encounter)
+    end
+
+    self.stage:addChild(self.battle)
+end
+
 function Game:encounter(encounter, transition, enemy, context)
     if r_includes(Registry.getEncounter, encounter, MNLEncounter) then
-        if transition == nil then transition = true end
-
-        if self.battle then
-            error("Attempt to enter battle while already in battle")
-        end
-
-        if enemy and not isClass(enemy) then
-            self.encounter_enemies = enemy
-        else
-            self.encounter_enemies = {enemy}
-        end
-
-        self.state = "BATTLE"
-
-        self.battle = MNLBattle()
-
-        if context then
-            self.battle.encounter_context = context
-        end
-
-        if type(transition) == "string" then
-            self.battle:postInit(transition, encounter)
-        else
-            self.battle:postInit(transition and "TRANSITION" or "INTRO", encounter)
-        end
-
-        self.stage:addChild(self.battle)
+        return self:mnlEncounter(encounter, transition, enemy, context)
     else
         return super.encounter(self, encounter, transition, enemy, context)
     end
