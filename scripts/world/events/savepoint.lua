fd6ee074004143422db4576e35abab0005e2996f
Hyperboid <23138363+Hyperboid@users.noreply.github.com>
2025-05-13 11:19:44 -0400

    Support marker property on save block

--- scripts/world/events/savepoint.lua
+++ scripts/world/events/savepoint.lua
@@ -5,10 +5,16 @@ function event:init(data)
     super.init(self, data)
     self.sprite.path = "world/events/saveblock"
     self.reusable = true
+    local properties = data and data.properties or {}
+
+    self.marker = properties["marker"]
+    self.simple_menu = properties["simple"]
+    self.text_once = properties["text_once"]
+    self.heals = properties["heals"] ~= false
 end
 
 function event:doItem()
-    Game.world:openMenu(SaveMenu())
+    Game.world:openMenu(SaveMenu(self.marker))
 end
 
 return event
